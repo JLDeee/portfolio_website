e0b7b0df160aeefdbde47273eb6aff599af611cd
JLDeee <jackieluu00@gmail.com>
2024-01-07 21:27:00 -0500

    added home page

---
+++ components/Home.js
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom';
+
+function About() {
+    return(
+    <main className='container'>
+    <section className='aboutPage'>
+        <header>
+            <h1>Welcome to Gamer's Guild</h1>
+        </header>
+    </section>
+    </main>);
+}
+
+export default About;
--- index.js
+++ index.js
@@ -1,14 +1,1 @@
-import { Link } from 'react-router-dom';
-
-function About() {
-    return(
-    <main className='container'>
-    <section className='aboutPage'>
-        <header>
-            <h1>Welcome to Gamer's Guild</h1>
-        </header>
-    </section>
-    </main>);
-}
-
-export default About;
+import './index.css';
